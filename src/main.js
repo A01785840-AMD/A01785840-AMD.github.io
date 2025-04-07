719c037f7e8530dfb9ef331dbd8a9d49a9e815ac
Angel Montemayor Davila <204801005+A01785840-AMD@users.noreply.github.com>
2025-04-07 12:09:12 -0600

    Add data links

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ const renders = {
     <div>
         <h1>HELLO WORLD!!</h1>
         <p>By Angel MD</p>
-        <a href="/about" class="page-link">Click Me</a>
-        <a href="/game" class="page-link">Start Game</a>
+        <a href="/about" data-rl class="page-link">Click Me</a>
+        <a href="/game" data-rl class="page-link">Start Game</a>
     </div>`
     },
     about: () => {
@@ -18,7 +18,7 @@ const renders = {
     <div>
         <h1>About Me!</h1>
         <p>By Angel MD</p>
-        <a href="/" class="page-link">Click Me</a>
+        <a href="/" data-rl class="page-link">Click Me</a>
     </div>`
     },
     not_found: () => {
@@ -26,12 +26,14 @@ const renders = {
         <div>
             <h1>Page not found 404.</h1>
             <p>By Angel MD</p>
-            <a href="/" class="page-link">Click Me</a>
+            <a href="/" data-rl class="page-link">Click Me</a>
         </div>`
     },
 }
 
-const router = new Router();
+const router = new Router("", {
+    linkTag: "rl"
+});
 
 router
     .on('/', renders.home)
